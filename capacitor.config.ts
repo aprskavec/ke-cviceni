35ddcb846c2cc548d7825e0096b1817ad249f6be
Adam Prskavec <43365243+aprskavec@users.noreply.github.com>
2026-02-12 12:31:34 +0000

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.dbfac7ada98641bb95fc805516ca345e',
+  appName: 'A Lovable project',
+  webDir: 'dist',
+  server: {
+    url: 'https://dbfac7ad-a986-41bb-95fc-805516ca345e.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
